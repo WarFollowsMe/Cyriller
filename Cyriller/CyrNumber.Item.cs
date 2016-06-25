c8b326917633c76010699c635cf3fc2354516409
WarFollowsMe <worfollomey@gmail.com>
2016-06-25 04:56:05 +0300

    обавлен некоторый функионал работы с фразами

--- Cyriller/CyrNumber.Item.cs
+++ Cyriller/CyrNumber.Item.cs
@@ -22,6 +22,16 @@ namespace Cyriller
                 this.plural = noun.DeclinePlural();
             }
 
+            public Item(CyrNoun Noun, string adjValue)
+            {
+                noun = Noun;
+                var adj = new CyrAdjectiveCollection().Get(adjValue, GetConditionsEnum.Similar, noun.Gender);
+                singular = adj.Decline(noun.Animate);
+                plural = adj.DeclinePlural(noun.Animate);
+                singular.Add(noun.Decline());
+                plural.Add(noun.DeclinePlural());
+            }
+
             public string[] GetName(CasesEnum Case, long Value)
             {
                 if (this.Gender == GendersEnum.Feminine)
@@ -61,17 +71,17 @@ namespace Cyriller
                     switch (Case)
                     {
                         case CasesEnum.Nominative:
-                            return new string[] { singular[1], singular[2], plural[2] };
+                            return new string[] { singular[1].Replace("-", " "), singular[2].Replace("-", " "), plural[2].Replace("-", " ") };
                         case CasesEnum.Genitive:
-                            return new string[] { singular[2], plural[2], plural[2] };
+                            return new string[] { singular[2].Replace("-", " "), plural[2].Replace("-", " "), plural[2].Replace("-", " ") };
                         case CasesEnum.Dative:
-                            return new string[] { singular[3], plural[3], plural[3] };
+                            return new string[] { singular[3].Replace("-", " "), plural[3].Replace("-", " "), plural[3].Replace("-", " ") };
                         case CasesEnum.Accusative:
-                            return new string[] { singular[4], plural[4], plural[4] };
+                            return new string[] { singular[4].Replace("-", " "), plural[4].Replace("-", " "), plural[4].Replace("-", " ") };
                         case CasesEnum.Instrumental:
-                            return new string[] { singular[5], plural[5], plural[5] };
+                            return new string[] { singular[5].Replace("-", " "), plural[5].Replace("-", " "), plural[5].Replace("-", " ") };
                         case CasesEnum.Prepositional:
-                            return new string[] { singular[6], plural[6], plural[6] };
+                            return new string[] { singular[6].Replace("-", " "), plural[6].Replace("-", " "), plural[6].Replace("-", " ") };
                         default:
                             throw new ArgumentException("Invalid Case!");
                     }
@@ -83,30 +93,30 @@ namespace Cyriller
                         case CasesEnum.Nominative:
                             if (noun.Name.ToLower() == "год")
                             {
-                                return new string[] { singular[1], singular[2], "лет" };
+                                return new string[] { singular[1].Replace("-", " "), singular[2].Replace("-", " "), "лет" };
                             }
 
-                            return new string[] { singular[1], singular[2], plural[2] };
+                            return new string[] { singular[1].Replace("-", " "), singular[2].Replace("-", " "), plural[2].Replace("-", " ") };
                         case CasesEnum.Genitive:
                             if (noun.Name.ToLower() == "год")
                             {
-                                return new string[] { singular[2], plural[2], "лет" };
+                                return new string[] { singular[2].Replace("-", " "), plural[2].Replace("-", " "), "лет" };
                             }
 
-                            return new string[] { singular[2], plural[2], plural[2] };
+                            return new string[] { singular[2].Replace("-", " "), plural[2].Replace("-", " "), plural[2].Replace("-", " ") };
                         case CasesEnum.Dative:
-                            return new string[] { singular[3], plural[3], plural[3] };
+                            return new string[] { singular[3].Replace("-", " "), plural[3].Replace("-", " "), plural[3].Replace("-", " ") };
                         case CasesEnum.Accusative:
                             if (noun.Name.ToLower() == "год")
                             {
-                                return new string[] { singular[4], singular[4], "лет" };
+                                return new string[] { singular[4].Replace("-", " "), singular[4].Replace("-", " "), "лет" };
                             }
 
-                            return new string[] { singular[1], singular[2], plural[2] };
+                            return new string[] { singular[1].Replace("-", " "), singular[2].Replace("-", " "), plural[2].Replace("-", " ") };
                         case CasesEnum.Instrumental:
-                            return new string[] { singular[5], plural[5], plural[5] };
+                            return new string[] { singular[5].Replace("-", " "), plural[5].Replace("-", " "), plural[5].Replace("-", " ") };
                         case CasesEnum.Prepositional:
-                            return new string[] { singular[6], plural[6], plural[6] };
+                            return new string[] { singular[6].Replace("-", " "), plural[6].Replace("-", " "), plural[6].Replace("-", " ") };
                         default:
                             throw new ArgumentException("Invalid Case!");
                     }
@@ -118,17 +128,17 @@ namespace Cyriller
                 switch (Case)
                 {
                     case CasesEnum.Nominative:
-                        return new string[] { singular[1], singular[2], plural[2] };
+                        return new string[] { singular[1].Replace("-", " ").Replace("-", " "), singular[2].Replace("-", " "), plural[2].Replace("-", " ") };
                     case CasesEnum.Genitive:
-                        return new string[] { singular[2], plural[2], plural[2] };
+                        return new string[] { singular[2].Replace("-", " "), plural[2].Replace("-", " "), plural[2].Replace("-", " ") };
                     case CasesEnum.Dative:
-                        return new string[] { singular[3], plural[3], plural[3] };
+                        return new string[] { singular[3].Replace("-", " "), plural[3].Replace("-", " "), plural[3].Replace("-", " ") };
                     case CasesEnum.Accusative:
-                        return new string[] { singular[4], singular[2], plural[4] };
+                        return new string[] { singular[4].Replace("-", " "), singular[2].Replace("-", " "), plural[4].Replace("-", " ") };
                     case CasesEnum.Instrumental:
-                        return new string[] { singular[5], plural[5], plural[5] };
+                        return new string[] { singular[5].Replace("-", " "), plural[5].Replace("-", " "), plural[5].Replace("-", " ") };
                     case CasesEnum.Prepositional:
-                        return new string[] { singular[6], plural[6], plural[6] };
+                        return new string[] { singular[6].Replace("-", " "), plural[6].Replace("-", " "), plural[6].Replace("-", " ") };
                     default:
                         throw new ArgumentException("Invalid Case!");
                 }
@@ -139,17 +149,17 @@ namespace Cyriller
                 switch (Case)
                 {
                     case CasesEnum.Nominative:
-                        return new string[] { singular[1], singular[2], plural[2] };
+                        return new string[] { singular[1].Replace("-", " "), singular[2].Replace("-", " "), plural[2].Replace("-", " ") };
                     case CasesEnum.Genitive:
-                        return new string[] { singular[2], plural[2], plural[2] };
+                        return new string[] { singular[2].Replace("-", " "), plural[2].Replace("-", " "), plural[2].Replace("-", " ") };
                     case CasesEnum.Dative:
-                        return new string[] { singular[3], plural[3], plural[3] };
+                        return new string[] { singular[3].Replace("-", " "), plural[3].Replace("-", " "), plural[3].Replace("-", " ") };
                     case CasesEnum.Accusative:
-                        return new string[] { singular[4], singular[2], plural[2] };
+                        return new string[] { singular[4].Replace("-", " "), singular[2].Replace("-", " "), plural[2].Replace("-", " ") };
                     case CasesEnum.Instrumental:
-                        return new string[] { singular[5], plural[5], plural[5] };
+                        return new string[] { singular[5].Replace("-", " "), plural[5].Replace("-", " "), plural[5].Replace("-", " ") };
                     case CasesEnum.Prepositional:
-                        return new string[] { singular[6], plural[6], plural[6] };
+                        return new string[] { singular[6].Replace("-", " "), plural[6].Replace("-", " "), plural[6].Replace("-", " ") };
                     default:
                         throw new ArgumentException("Invalid Case!");
                 }
